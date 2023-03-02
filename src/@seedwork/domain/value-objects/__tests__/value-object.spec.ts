45f90177cf5290ee8435c3a8d84e5ac299e96c13
Rodrigo Souza <rodrigo.souza@mercadofavo.com>
2023-03-02 21:20:10 +0000

    feat: value object as a immutable object

--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -1,15 +1,53 @@
 import { ValueObject } from "../value-object";
 
-class StubValueObject extends ValueObject {
+class StubValueObject extends ValueObject {}
 
-}
+describe("ValueObject Unit Tests", () => {
+  it("should set value", () => {
+    let vo = new StubValueObject("string value");
+    expect(vo.value).toBe("string value");
 
-describe('ValueObject Unit Tests', () => {
-  it('should set value', () => {
-    let vo = new StubValueObject('string value');
-    expect(vo.value).toBe('string value');
+    vo = new StubValueObject({ prop1: "value1" });
+    expect(vo.value).toStrictEqual({ prop1: "value1" });
+  });
 
-    vo = new StubValueObject({ prop1: 'value1' });
-    expect(vo.value).toStrictEqual({ prop1: 'value1' });
+  it("should convert to a string", () => {
+    const date = new Date();
+    const arrange = [
+      { received: 0, expected: "0" },
+      { received: 1, expected: "1" },
+      { received: 5, expected: "5" },
+      { received: true, expected: "true" },
+      { received: false, expected: "false" },
+      { received: date, expected: date.toString() },
+      {
+        received: { prop1: "value1" },
+        expected: JSON.stringify({ prop1: "value1" }),
+      },
+    ];
+
+    arrange.forEach((value) => {
+      const vo = new StubValueObject(value.received);
+      expect(vo + "").toBe(value.expected);
+    });
+  });
+
+  it("should return a immutable object", () => {
+    const obj = {
+      prop1: "value1",
+      deep: { prop2: "value2", prop3: new Date() },
+    };
+
+    const vo = new StubValueObject(obj)
+
+    expect(() => {
+     (vo as any).value.prop1 = 'changed'
+    }).toThrow("Cannot assign to read only property 'prop1' of object '#<Object>'");
+
+    expect(() => {
+      (obj as any).deep.prop2 = 'changed'
+    }).toThrow("Cannot assign to read only property 'prop2' of object '#<Object>'");
+
+    expect(vo.value.deep.prop3).toBeInstanceOf(Date);
   });
 });
